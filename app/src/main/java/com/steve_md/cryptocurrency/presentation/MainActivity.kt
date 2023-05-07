ecce1dfce001aae22e882937944b6abf4a2c409c
MuindiStephen <stephenmuindi241@gmail.com>
2023-05-07 14:12:13 +0300

    feature/added packages and clean architecture set-up

---
+++ app/src/main/java/com/steve_md/cryptocurrency/presentation/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.steve_md.cryptocurrency.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.steve_md.cryptocurrency.presentation.ui.theme.CryptoCurrencyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoCurrencyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+
